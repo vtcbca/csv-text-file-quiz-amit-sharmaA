faa7e439fb590acb68a86d5af608affa03b71cff
22bca02@vtcbcsr.edu.in <128131333+amit-sharmaA@users.noreply.github.com>
2023-08-21 11:09:31 +0530

    Add files via upload
    QUIZ 1

---
+++ QUIZ 1.py
@@ -0,0 +1,41 @@
+QUS:-3
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+i=['BCA course','Bachelor of computer Application','is an undergraduate degree program that focuses on technology and computer applications','Spanning three years, this course equips students with the knowledge and skills required to excel in the field of computer science','I also chooes BCA course becuses they are so high course on this time.']
+
+
+# In[12]:
+# WRITER
+
+with open('E:\\22bca02\\python\\BCA.txt','w') as f:
+    for line in i:
+        f.write(line)
+        f.write('\n')
+
+
+# In[13]:
+#READER
+
+
+with open('E:\\22bca02\\python\\BCA.txt','r') as f:
+    c=f.read()
+    print(c)
+
+
+# In[ ]:
+ #OUTPUT
+
+BCA course
+Bachelor of computer Application
+is an undergraduate degree program that focuses on technology and computer applications
+Spanning three years, this course equips students with the knowledge and skills required to excel in the field of computer science
+I also chooes BCA course becuses they are so high course on this time.
+
+
+
+
